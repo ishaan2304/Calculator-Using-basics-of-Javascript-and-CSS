1966655356237a40d32058563317ebf2831acb07
Ishan Nalawade <125126646+ishaan2304@users.noreply.github.com>
2025-03-16 11:40:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// Get the HTML element with the ID "display" and store it in the variable `display`.
-const display = document.getElementById("display");
-
-// Function to append a given input to the display
-function appendToDisplay(input) {
-    // Add the input value to the current value of the display
-    display.value += input;
+const display=document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input
 }
-
-// Function to clear the display
-function clearDisplay() {
-    // Set the display value to an empty string, effectively clearing it
-    display.value = "";
+function clearDisplay(){
+    display.value="";
 }
-
-// Function to evaluate the expression entered in the display
-funct
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(err){
+        display.value="error";
+    }
+}
